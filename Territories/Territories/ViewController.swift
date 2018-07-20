d46858a57632cf90f2ae9c9f90e138eaf4f01413
Vadini Agrawal <vadini.agrawal@sap.com>
2018-07-19 17:31:34 -0700

    Added folder

---
+++ Territories/Territories/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Territories
+//
+//  Created by Agrawal, Vadini on 7/19/18.
+//  Copyright © 2018 Agrawal, Vadini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
